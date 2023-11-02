21a580a67b7dcec57ea393673ef24e0578079a0a
Raiyan Al Sultan <rraiyan77@gmail.com>
2023-11-02 20:58:43 +0600

    Finding and recovering missing values

--- hear_attack_data_set.R
+++ hear_attack_data_set.R
@@ -1,3 +1,20 @@
 
 heart_attack_ds <- read.csv("E:/Courses/AIUB Courses/9th Semester/Introduction to data science/Midterm/Midterm Project/heart_attack_ds.csv")
 heart_attack_ds
+
+head(heart_attack_ds)
+tail(heart_attack_ds)
+summary(heart_attack_ds)
+str(heart_attack_ds)
+dim(heart_attack_ds)
+
+colSums(is.na(heart_attack_ds))
+
+
+mean_age <- mean(heart_attack_ds$Age, na.rm = TRUE)
+heart_attack_ds$Age[is.na(heart_attack_ds$Age)] <- mean_age
+
+
+median_age <- median(heart_attack_ds$Age, na.rm = TRUE)
+heart_attack_ds$Age[is.na(heart_attack_ds$Age)] <- median_age
+
