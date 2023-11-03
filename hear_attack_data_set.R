575a8946d119a11ee1070062d3579a491e2da8a6
Raiyan Al Sultan <rraiyan77@gmail.com>
2023-11-04 02:34:27 +0600

    Exploring data set

--- hear_attack_data_set.R
+++ hear_attack_data_set.R
@@ -1,44 +1,12 @@
-
-
 options(max.print = .Machine$integer.max)
 
 heart_attack_ds <- read.csv("E:/Courses/AIUB Courses/9th Semester/Introduction to data science/Midterm/Midterm Project/heart_attack_ds.csv")
 heart_attack_ds
-
 head(heart_attack_ds)
 tail(heart_attack_ds)
 summary(heart_attack_ds)
 str(heart_attack_ds)
+
 dim(heart_attack_ds)
 
-# Finding missing values
-colSums(is.na(heart_attack_ds))
-
-#Recovering missing values using mean
-mean_age <- mean(heart_attack_ds$Age, na.rm = TRUE)
-heart_attack_ds$Age[is.na(heart_attack_ds$Age)] <- mean_age
-
-#Recovering missing values using median
-median_age <- median(heart_attack_ds$Age, na.rm = TRUE)
-heart_attack_ds$Age[is.na(heart_attack_ds$Age)] <- median_age
-
-
-#---------------------------
-library(dplyr)
-
-# Data value transformation in Sex attribute
-# Assuming 'heart_data' is your data set
-heart_attack_ds <- heart_attack_ds %>%
-  mutate(Sex = recode(Sex, "M" = 1, "F" = 0))
-heart_attack_ds
-
-
-----------------------
-  
-# Calculate the mode of the "Sex" variable
-mode_sex <- as.character(which.max(table(heart_attack_ds$Sex)))
-mode_sex
-# Impute missing "Sex" values with the mode
-heart_attack_ds$Sex[is.na(heart_attack_ds$Sex)] <- mode_sex
-heart_attack_ds
 
